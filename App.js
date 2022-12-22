01113ff1de91920573d936a8b1428542efc4cb12
David Lindberg <david.wesley.lindberg@gmail.com>
2022-12-22 14:23:20 -0600

    moved the image to its own component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
+import ImageViewer from "./components/ImageViewer";
 
 const placeHolderImage = require('./assets/images/background-image.png')
 
@@ -7,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={placeHolderImage} style={styles.image}/>
+        <ImageViewer placeholderImageSource={placeHolderImage}/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -23,10 +24,5 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
   },
 });
